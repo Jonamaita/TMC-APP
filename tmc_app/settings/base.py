f55bbcebd3af1cbdf2cb78582d9a73ab2b5e7b2a
jonathan Maita <jonamaita@gmail.com>
2020-06-29 01:22:19 -0400

    chore: add new config for database

--- tmc_app/settings/base.py
+++ tmc_app/settings/base.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tmc_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cumplo_tmc',
+        'USER': 'postgres',
+        'PASSWORD': 'maita',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
